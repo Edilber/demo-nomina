804369405437b837b02bfa9f2322c13dcc9daf05
Edilber <eddylber@gmail.com>
2020-11-01 20:02:03 -0600

    Se agregó la clase HourlyEmployee

---
+++ HourlyEmployee.cs
@@ -0,0 +1,11 @@
+class HourlyEmployee : Employee
+{
+	public HourlyEmployee(string EmployeeId, string firstName, string lastName, int age, double payRate) : base (EmployeeId, firstName, lastName, age, payRate)
+	{}
+	
+	public override double CalculatePay(int hoursWorked)
+	{
+		Console.WriteLine("HourlyEmployee.CalculatePay");
+		return 42; //valor imaginario
+	}
+}
