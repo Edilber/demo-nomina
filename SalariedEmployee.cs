feed1674da9db47ad63e6186548f160456927090
Edilber <eddylber@gmail.com>
2020-11-01 18:06:35 -0600

    Se agregó la clase SalariedEmployee

---
+++ SalariedEmployee.cs
@@ -0,0 +1,11 @@
+class SalariedEmployee : Employee
+{
+	public string SocialSecurityNumber;
+	
+	public void CalculatePay(int hoursWorked)
+	{
+		//Calcular el pago para el empleado W-2
+		
+	}
+	
+}
